1a85906ca1838e266cfbd5b15e3cc6db71691378
cavargar <cavargar>
2021-10-14 18:56:22 +0000

    Cuaderno comandos básicos Planetary Computer

---
+++ pc_basics.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Introducción a Planetary Computer 
+# MAGIC Este cuaderno de Databricks busca ser una introducción a los comandos básicos y el acceso a la información satelital de los sensores disponibles en la recientemente lanzada plataforma <a href=https://planetarycomputer.microsoft.com/>Planetary Computer </a>. El objetivo es que las entidades de nuestro gobierno colombiano puedan empezar a usar esta herramienta usando servicios de la nube, bien sea contratados por la misma entidad o a través de iniciativas como el <a href=https://sandbox.datos.gov.co/#!/inicio>Data Sandbox de MinTic </a>.
+
+# COMMAND ----------
+
+
